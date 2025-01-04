eaacfe853a71c073cdb754301111a53e15289b95
Ambrose Blay <ambrose2002blay@gmail.com>
2025-01-03 22:48:02 -0500

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Ambrose Blay on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
